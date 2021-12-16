b7cf7193eaf2fb3473591cda05b8f91226e413cb
rousean <rousean@163.com>
2021-12-16 13:41:27 +0800

    更新

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
-const router = require('koa-router')()
+const router = require("koa-router")()
 
-router.get('/', async (ctx, next) => {
-  await ctx.render('index', {
-    title: '个人博客后端接口',
+router.get("/", async (ctx, next) => {
+  await ctx.render("index", {
+    title: "个人博客后端接口！",
   })
 })
 
