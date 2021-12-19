887c924db3993b81833a6e64584120f37ecdc2a0
rousean <rousean@163.com>
2021-12-19 20:04:32 +0800

    更新

--- config/config.js
+++ config/config.js
@@ -1,15 +1,6 @@
 "use strict"
 
 // 全局配置
-const config = {
-  mongodb: "mongodb://127.0.0.1:27017/blog",
-  mysql: {
-    host: "127.0.0.1", // IP
-    port: 3306, // 端口号
-    database: "blog", // 数据库名
-    user: "root", // 数据库用户名
-    password: "", // 数据库密码
-  },
-}
+const config = { mongodb: "mongodb://127.0.0.1:27017/blog" }
 
 module.exports = config
